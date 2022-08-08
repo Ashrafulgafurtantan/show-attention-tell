8da99cb4881c6def7b80459a95298aea6dd48dcc
Ashraful Gafur <ashraful.gafur@streamstech.com>
2022-08-08 10:33:55 +0600

    print and pickle modified

--- core/bleu.py
+++ core/bleu.py
@@ -1,4 +1,4 @@
-import cPickle as pickle
+import pickle
 import os
 import sys
 sys.path.append('../coco-caption')
@@ -24,48 +24,47 @@ def score(ref, hypo):
             final_scores[method] = score
 
     return final_scores
-    
+
 
 def evaluate(data_path='./data', split='val', get_scores=False):
     reference_path = os.path.join(data_path, "%s/%s.references.pkl" %(split, split))
     candidate_path = os.path.join(data_path, "%s/%s.candidate.captions.pkl" %(split, split))
-    
+
     # load caption data
     with open(reference_path, 'rb') as f:
         ref = pickle.load(f)
     with open(candidate_path, 'rb') as f:
         cand = pickle.load(f)
-    
+
     # make dictionary
     hypo = {}
     for i, caption in enumerate(cand):
         hypo[i] = [caption]
-    
+
     # compute bleu score
     final_scores = score(ref, hypo)
 
     # print out scores
-    print 'Bleu_1:\t',final_scores['Bleu_1']  
-    print 'Bleu_2:\t',final_scores['Bleu_2']  
-    print 'Bleu_3:\t',final_scores['Bleu_3']  
-    print 'Bleu_4:\t',final_scores['Bleu_4']  
-    print 'METEOR:\t',final_scores['METEOR']  
-    print 'ROUGE_L:',final_scores['ROUGE_L']  
-    print 'CIDEr:\t',final_scores['CIDEr']
-    
+    print('Bleu_1:\t',final_scores['Bleu_1'])
+    print('Bleu_2:\t',final_scores['Bleu_2'])
+    print('Bleu_3:\t',final_scores['Bleu_3'])
+    print('Bleu_4:\t',final_scores['Bleu_4'])
+    print('METEOR:\t',final_scores['METEOR'])
+    print('ROUGE_L:',final_scores['ROUGE_L'])
+    print('CIDEr:\t',final_scores['CIDEr'])
     if get_scores:
         return final_scores
-    
-   
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+
+
+
 
 
